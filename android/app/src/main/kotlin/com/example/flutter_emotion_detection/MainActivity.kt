5def0608e57c7b5b7cd2e23e171ea2f5818b6aa8
ZAFuad <zuhayerfuad@gmail.com>
2023-03-17 22:15:38 +0600

    1 bug has to be fixed

---
+++ android/app/src/main/kotlin/com/example/flutter_emotion_detection/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.flutter_emotion_detection
+
+
+import io.flutter.embedding.android.FlutterActivity;
+
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_emotion_detection/home.dart';
+
+List<CameraDescription>? cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+    );
+  }
+}
